feb06df0de5d1e387b29076b5c97f8e2cef74aa6
Nils Hansson <nilsmenghansson@gmail.com>
2024-08-12 13:01:59 +0200

    installed ddrizzle and created server folder

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -1,7 +1,7 @@
 export function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
-      <div>Gallery</div>
+      <div>ClosetClear</div>
 
       <div className="flex flex-row items-center gap-4">
         <h1>signin</h1>
