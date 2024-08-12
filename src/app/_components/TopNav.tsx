9744d09583b89145efe8428084e25a19dceb2c3d
Nils Hansson <nilsmenghansson@gmail.com>
2024-08-12 11:51:31 +0200

    set up topnav

---
+++ src/app/_components/TopNav.tsx
@@ -0,0 +1,18 @@
+export function TopNav() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+      <div>Gallery</div>
+
+      <div className="flex flex-row items-center gap-4">
+        <h1>signin</h1>
+        {/* <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <SimpleUploadButton />
+          <UserButton />
+        </SignedIn> */}
+      </div>
+    </nav>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { TopNav } from "./_components/TopNav";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <TopNav />
       <body className={inter.className}>{children}</body>
     </html>
   );
