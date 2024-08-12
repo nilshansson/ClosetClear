8ddc61dd33fe9f62f78ff98d115fec78fc7ba3b8
Nils Hansson <nilsmenghansson@gmail.com>
2024-08-12 14:42:54 +0200

    implemented uploadthing

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import { UploadButton } from "../utils/uploadthing";
 export function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
@@ -11,6 +12,7 @@ export function TopNav() {
           <SignInButton />
         </SignedOut>
         <SignedIn>
+          <UploadButton endpoint="imageUploader" />
           <UserButton />
         </SignedIn>
       </div>
---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,33 @@
+import { auth, clerkClient } from "@clerk/nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+import { db } from "@/app/server/db";
+import { product } from "@/app/server/db/schema";
+
+const f = createUploadthing();
+
+export const ourFileRouter = {
+  imageUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 40 } })
+    .middleware(async ({ req }) => {
+      const user = auth();
+      if (!user.userId) throw new UploadThingError("Unauthorized");
+
+      const fullUserData = await clerkClient.users.getUser(user.userId);
+
+      if (fullUserData?.privateMetadata?.["can-upload"] !== true)
+        throw new UploadThingError("User Does Not Have Upload Permissions");
+
+      return { userId: user.userId };
+    })
+    .onUploadComplete(async ({ metadata, file }) => {
+      await db.insert(product).values({
+        name: file.name,
+        url: file.url,
+        userId: metadata.userId,
+      });
+
+      return { uploadedBy: metadata.userId };
+    }),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
