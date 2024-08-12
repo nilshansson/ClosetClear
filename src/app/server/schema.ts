c76d379c7538509f67c00b62f850d37375136634
Nils Hansson <nilsmenghansson@gmail.com>
2024-08-12 13:05:33 +0200

    created mockup schema with drizzle

--- src/app/server/schema.ts
+++ src/app/server/schema.ts
@@ -1,1 +1,23 @@
-export function Schema() {}
+import { sql } from "drizzle-orm";
+import {
+  pgTableCreator,
+  serial,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const createTable = pgTableCreator((name) => `t3gallery_${name}`);
+
+export const product = createTable("product", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 256 }).notNull(),
+  url: varchar("url", { length: 1024 }).notNull(),
+
+  userId: varchar("userId", { length: 256 }).notNull(),
+
+  createdAt: timestamp("created_at")
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp("updatedAt"),
+  expiringAt: timestamp("expiringAt"),
+});
