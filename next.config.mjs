de7cc7a338af7d01879b6cf39381529e79c87d7b
Nils Hansson <nilsmenghansson@gmail.com>
2024-08-15 13:59:18 +0200

    try again

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
-import path from "path";
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve("src"), // Using ES module syntax for path
-    };
-    return config;
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
